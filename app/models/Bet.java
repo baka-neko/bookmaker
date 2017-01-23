06a358e16414f1a853277ad0e624b2e1c854b0df
Alex Morro <thebocman@gmail.com>
2017-01-24 00:14:53 +0300

    initial commit

---
+++ app/models/Bet.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Spek
+ * Date: 24.01.17
+ * Time: 0:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bet {
+}
